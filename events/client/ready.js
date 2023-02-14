11ebd17655ac3d07d0ff766a2719239778fc67e5
Morar Andrei - Alexandru <whiteex@icloud.com>
2023-02-14 23:16:10 +0200

    February 14th, 2023
    - Change the log of the ready event

--- events/client/ready.js
+++ events/client/ready.js
@@ -7,6 +7,6 @@ module.exports = {
 
 client.on('ready', () => {
     loadCommands(client);
-    console.log(`${client.user.username}` + ' ' + 'is now online.');
+    console.log(`${client.user.username}` + ' ' + 'successfully logged in.');
     client.user.setActivity(`on ${client.guilds.cache.size} server(s).`);
 });
